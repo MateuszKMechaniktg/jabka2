46e2d0000c1bb631cd8ef7e93ab99e0d0c9a7441
mateuszkmechaniktg <mateusz.kubica@uczen.mechaniktg.pl>
2026-03-27 11:36:24 +0100

    koniec

--- app/src/main/java/com/pt2/myapplication/MainActivity.java
+++ app/src/main/java/com/pt2/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -21,12 +22,15 @@ public class MainActivity extends AppCompatActivity {
     TextView czas;
     TextView punkty;
     Button start;
-    private int ileSekund = 60;
+    int ileSekund;
     int punktyLiczba = 0;
     private CountDownTimer countDownTimer;
     GridLayout grid;
     View[] japka;
     int random;
+    int zycieJapka;
+    TextView koniec;
+    LinearLayout czasLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         czas = findViewById(R.id.czas);
         punkty = findViewById(R.id.punkty);
         grid = findViewById(R.id.grid);
+        koniec = findViewById(R.id.koniec);
+        czasLayout = findViewById(R.id.czasLayout);
         japka = new View[] {
                 findViewById(R.id.j1),
                 findViewById(R.id.j2),
@@ -65,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                             random = new Random().nextInt(9);
                             japka[random].setVisibility(View.VISIBLE);
+                            zycieJapka = ileSekund;
                         }
                     }
             );
@@ -75,20 +82,43 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        punkty.setText("0");
+                        ileSekund = 60;
+                        grid.setVisibility(View.VISIBLE);
+                        czasLayout.setVisibility(View.VISIBLE);
                         start.setVisibility(View.INVISIBLE);
                         random = new Random().nextInt(9);
                         japka[random].setVisibility(View.VISIBLE);
+                        koniec.setVisibility(View.GONE);
+                        zycieJapka = ileSekund;
 
                         countDownTimer = new CountDownTimer(ileSekund * 1000, 1000) {
                             @Override
                             public void onTick(long l) {
                                 ileSekund = (int) (l/1000);
+                                if (zycieJapka >= ileSekund + 2){
+                                    japka[random].setVisibility(View.INVISIBLE);
+
+                                    japka[random].setVisibility(View.INVISIBLE);
+                                    start.setVisibility(View.VISIBLE);
+                                    countDownTimer.cancel();
+                                    grid.setVisibility(View.GONE);
+                                    koniec.setVisibility(View.VISIBLE);
+                                    czasLayout.setVisibility(View.INVISIBLE);
+                                    punktyLiczba = 0;
+                                }
                                 czas.setText(ileSekund+"s");
                             }
 
                             @Override
                             public void onFinish() {
+                                japka[random].setVisibility(View.INVISIBLE);
                                 start.setVisibility(View.VISIBLE);
+                                countDownTimer.cancel();
+                                grid.setVisibility(View.GONE);
+                                koniec.setVisibility(View.VISIBLE);
+                                czasLayout.setVisibility(View.INVISIBLE);
+                                punktyLiczba = 0;
                             }
                         };
                         countDownTimer.start();
